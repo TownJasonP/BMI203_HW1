be6161769fc86c4236bfcf54676d1f12aea02f03
TownJasonP <student@CCB2016-User1.local>
2017-01-24 12:43:01 -0800

    adding more tests

--- test/test.py
+++ test/test.py
@@ -7,18 +7,15 @@ def test_bubblesort():
     # think about: (1) does your code handle 0-element arrays without
     # failing, (2) does your code handle characters?
 
+    #Basic check - does it work?
     x = np.array([1,2,4,0,1])
-
-    # for now, just attempt to call the bubblesort function, should
-    # actually check output
     np.testing.assert_equal(bubblesort(x),[0,1,1,2,4])
-
+    #Check to make sure it can handle 1 element lists
     x = np.array([0])
-
-    # for now, just attempt to call the bubblesort function, should
-    # actually check output
     np.testing.assert_equal(bubblesort(x),[0])
-
+    #Check to make sure it can handle 0 element lists
+    x = []
+    np.testing.assert_equal(bubblesort(x),[])
 
 def test_quicksort():
 
